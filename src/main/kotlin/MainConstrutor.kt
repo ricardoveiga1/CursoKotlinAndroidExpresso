f6795a4fe5265b1af0d20a53f3b5f993082aa5b9
ricardoveiga1 <ricardoveiga.ti@gmail.com>
2024-04-17 00:38:33 -0300

    bloco init e propriedades do objeto

--- src/main/kotlin/MainConstrutor.kt
+++ src/main/kotlin/MainConstrutor.kt
@@ -6,18 +6,27 @@ fun main() {
     //sobercarga de cosntrutor
 
 
-    val userA = UserConstructor("Ricardo Veiga", true)
-    val nameLentgh = userA.getameLength()
-    println(nameLentgh)
-    println(userA.name)
-    println(userA.isAdmin)
+//    val userA = UserConstructor("Ricardo Veiga", true)
+//    val nameLentgh = userA.getameLength()
+//    println(nameLentgh)
+//    println(userA.name)
+//    println(userA.isAdmin)
+//
+//
+//    val userB = UserConstructor("Gustavo")
+//    //userB.name = "Gustavo"
+//    println(userB.getameLength())
+//    println(userB.name)
+//    println(userB.isAdmin)
 
 
-    val userB = UserConstructor("Gustavo")
-    //userB.name = "Gustavo"
-    println(userB.getameLength())
-    println(userB.name)
-    println(userB.isAdmin)
+    //Testando INIT dentro da pilha de construtores, comenta código acima e execute
+    val user = UserConstructor()
+
+
+    // PROPRIEDADES DO OBJETO
+    //user.label = "Trocar o texto da label"
+    println(user.label)
 
 
 
--- src/main/kotlin/UserConstructor.kt
+++ src/main/kotlin/UserConstructor.kt
@@ -7,10 +7,29 @@
 //OPÇÃO 3 - espera que o construtor receba o valor(var)
 class UserConstructor constructor( var name: String, var isAdmin:Boolean) {
 
+    //var - mutavel
+    //val - imutavel
+    var label = "$name é admin do sistema: $isAdmin"
+
+
+    // Tiago falou: INIT é como construtor (só que não tem parametros) e é executado depois do construtor padrão
+    //o INIT é usado para inicializar log, banco de dados, ele é sempre utilizado para executar entre a PILHA de construtores(construtores não importa a ordem, a ordem é no momento da chamada da instancia passando parametro de cada cosntrutor)
+    init{
+        println("Olá INIT")
+    }
+
+    init{
+        println("Olá INIT 2") // a Ordem importa, mas geralmente não usamos mais de um INIT, o chamado multinitialization
+    }
+
     //construtor 2 - sobrecarga de construtor
     constructor(name: String): this(name, false) //direcionando para o primeiro construtor, quando chama esse, automaticamrnte chama o primieor construtor
     {
         println("Executei o construtor 2")
+    }
+
+    constructor(): this("Desconhecido"){
+        println("Executa o Construtor 3")
     }
 
 
